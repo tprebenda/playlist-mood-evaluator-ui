cef1f2f9b15f1f3a96c1e145ce98071fc816f638
Troy <tprebenda@gmail.com>
2024-06-04 14:06:59 -0400

    add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,26 @@
 // import logo from "./logo.svg";
 import OAuthCard from "./login";
 import "./App.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <OAuthCard />,
+  },
+  // {
+  //   path: "about",
+  //   element: <div>About</div>,
+  // },
+]);
 
 function App() {
-  return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-    <OAuthCard />
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
