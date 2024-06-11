108e230beab9f72b29040131d15b24978b7a9858
Troy <tprebenda@gmail.com>
2024-06-11 12:43:30 -0400

    add home page logo

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,20 +1,38 @@
 import Button from "@mui/material/Button";
-import { useNavigate } from "react-router-dom";
+import Grid from "@mui/material/Grid";
+import "./Home.css";
+import Typography from "@mui/material/Typography";
 
 interface TempHomeProps {
   signOut: () => void;
 }
 const Home = ({ signOut }: TempHomeProps) => {
   return (
-    <div style={{ display: "flex" }}>
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
       <Button
         size="large"
         variant="outlined"
-        sx={{ color: "blue" }}
+        sx={{ color: "blue", width: "15%" }}
         onClick={signOut}
       >
         Log out
       </Button>
+      <Grid
+        container
+        direction="column"
+        justifyContent="space-evenly"
+        alignItems="center"
+      >
+        <Grid item xs={4} m={3}>
+          <div className="circle pulse">
+            <Typography variant="h3" fontFamily="IBM Plex Sans Condensed">
+              Playlist Mood Evaluator
+            </Typography>
+          </div>
+        </Grid>
+      </Grid>
     </div>
   );
 };
