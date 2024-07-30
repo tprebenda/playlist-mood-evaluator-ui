ceff7503a10a96e18298178392c3494f04c9f828
Troy <tprebenda@gmail.com>
2024-07-30 18:10:53 -0400

    add background image for /about

--- src/views/about/About.tsx
+++ src/views/about/About.tsx
@@ -2,6 +2,8 @@ import Box from "@mui/material/Box";
 import AppLogo from "../../common/appLogo/AppLogo";
 import AppBarHeader from "../../common/appBar/AppBar";
 import Paper from "@mui/material/Paper";
+import synthWaveWallpaper from "../../assets/synthWaveWallpaper.jpg";
+import BackgroundImage from "../../common/backgroundImage/BackgroundImage";
 
 const aboutText = `The Playlist Mood Evaluator is a React web application developed by me, \
 Troy Prebenda.\nIt is designed to give users some insight into the makeup of their playlists, \
@@ -11,17 +13,28 @@ const About = () => {
   return (
     <>
       <AppBarHeader />
-      <Box
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <AppLogo />
-        <Paper elevation={2} sx={{ maxWidth: "500px", whiteSpace: "pre-line" }}>
-          <Box padding={2}>{aboutText}</Box>
-        </Paper>
-      </Box>
+      <BackgroundImage imageUrl={synthWaveWallpaper}>
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          height="50%"
+          width="30%"
+          sx={{
+            background: "black",
+            borderRadius: "10%",
+            border: "solid 1px",
+          }}
+        >
+          <AppLogo />
+          <Paper
+            elevation={2}
+            sx={{ maxWidth: "500px", whiteSpace: "pre-line" }}
+          >
+            <Box padding={2}>{aboutText}</Box>
+          </Paper>
+        </Box>
+      </BackgroundImage>
     </>
   );
 };
