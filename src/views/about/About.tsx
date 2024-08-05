d710846e4f07b2c07417b028ccf79c626da6aa7e
Troy <tprebenda@gmail.com>
2024-08-05 13:28:34 -0400

    add more info to /about

--- src/views/about/About.tsx
+++ src/views/about/About.tsx
@@ -3,10 +3,24 @@ import AppLogo from "../../common/appLogo/AppLogo";
 import AppBarHeader from "../../common/appBar/AppBar";
 import { lineRiderSynthWallpaper } from "../../assets/wallpapers";
 import BackgroundImage from "../../common/backgroundImage/BackgroundImage";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+let aboutTheme = createTheme({
+  typography: {
+    fontFamily: `"IBM Plex Sans Condensed", "Arial", "Good Times"`,
+    fontSize: 16,
+  },
+});
 
 const aboutText = `The Playlist Mood Evaluator is a React web application developed by me, \
 Troy Prebenda.\nIt is designed to give users some insight into the makeup of their playlists, \
-and to highlight the songs that share similar audio features.`;
+and to highlight the songs that share similar audio features.\n\nMusic has always been \
+a very meaningful part of my life - I played many instruments growing up, and I continue to \
+rely on music for inspiration while working, exercising, gaming, etc.\nI saw this project \
+as an opportunity to look deeper into the songs of my playlists, and to better understand my \
+taste in music and my overall 'style'.\nI hope you can use this tool to do the same for your \
+playlists!`;
 
 const About = () => {
   return (
@@ -17,7 +31,6 @@ const About = () => {
           display="flex"
           flexDirection="column"
           alignItems="center"
-          height="50%"
           width="30%"
           sx={{
             background: "black",
@@ -26,9 +39,11 @@ const About = () => {
           }}
         >
           <AppLogo />
-          <Box width="70%" padding={2}>
-            {aboutText}
-          </Box>
+          <ThemeProvider theme={aboutTheme}>
+            <Box width="70%" whiteSpace="pre-wrap" textAlign="center" mb={5}>
+              <Typography>{aboutText}</Typography>
+            </Box>
+          </ThemeProvider>
         </Box>
       </BackgroundImage>
     </>
