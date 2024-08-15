a935a00e5843bd72b2a60c80f42afa8e31ebf768
Troy <tprebenda@gmail.com>
2024-08-15 13:44:19 -0400

    remove fly files; update urls to use new domain!

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API_URL_BASE = "http://127.0.0.1:8000"; // LOCAL DEV
-export const API_URL_BASE = "https://playlist-mood-evaluator-api.fly.dev:8080";
+// export const API_URL_BASE = "http://127.0.0.1:8000"; // LOCAL DEV
+export const API_URL_BASE = "https://api.playlistmoodevaluator.com";
 
 export const axiosInstance = axios.create({
   withCredentials: true,
