cf43bb473454123d285e0070d1de29f7ac2fe28a
Troy <tprebenda@gmail.com>
2025-06-13 18:20:40 -0400

    update repo (without secrets)

--- src/helpers/auth/authHelpers.ts
+++ src/helpers/auth/authHelpers.ts
@@ -1,6 +1,6 @@
 // Used for Spotify OAuth Code flow:
 // https://developer.spotify.com/documentation/web-api/tutorials/code-flow
-const CLIENT_ID = "5b9ee404632b45f6a6d6cc35824554a6";
+const CLIENT_ID = "XXXXXX";
 
 // playlist-read-private playlist-read-collaborative: required to view playlists
 const SCOPE = "playlist-read-private playlist-read-collaborative";
